7647614cd3a6d8384234a665129eccb2874ef876
meltingseaweed <conor.seo@gmail.com>
2026-03-15 15:42:06 +0900

    Change class variabls to instance variables

--- Hashmap.rb
+++ Hashmap.rb
@@ -2,9 +2,12 @@ require 'pry-byebug'
 
 class HashMap
   
-  @@load_factor = 0.75
-  @@capacity = 16
-  @@hasharray = Array.new(@@capacity).map { |val| val = [] }
+  def initialize
+  @load_factor = 0.75
+  @capacity = 16
+  @hasharray = Array.new(@capacity).map { |val| val = [] }  
+  end
+  
 
   def hash(key)
     hash_code = 0
@@ -17,26 +20,26 @@ class HashMap
 
   def set(key, value)
     hash_code = hash(key)
-    bucket = hash_code % @@capacity
-    @@hasharray[bucket] << { key => value }
+    bucket = hash_code % @capacity
+    @hasharray[bucket] << { key => value }
 
-    if length > (@@load_factor * @@capacity)
-      @@capacity *= 2
+    if length > (@load_factor * @capacity)
+      @capacity *= 2
       entries_array = entries
-      @@hasharray = Array.new(@@capacity).map { |val| val = [] }
+      @hasharray = Array.new(@capacity).map { |val| val = [] }
       entries_array.each do |pair|
         hash_code = hash(pair[0])
-        bucket = hash_code % @@capacity
-        @@hasharray[bucket] << { pair[0] => pair[1] }
+        bucket = hash_code % @capacity
+        @hasharray[bucket] << { pair[0] => pair[1] }
       end
-      puts @@hasharray
+      puts @hasharray
     end
   end
 
   def get(key)
     hash_code = hash(key)
-    bucket = hash_code % @@capacity
-    pairs = @@hasharray[bucket]
+    bucket = hash_code % @capacity
+    pairs = @hasharray[bucket]
     keys_array = []
     pairs.each do | pair | 
       keys_array << pair.keys
@@ -51,8 +54,8 @@ class HashMap
 
   def has?(key)
     hash_code = hash(key)
-    bucket = hash_code % @@capacity
-    pairs = @@hasharray[bucket]
+    bucket = hash_code % @capacity
+    pairs = @hasharray[bucket]
     keys_array = []
     pairs.each  { | pair | keys_array << pair.keys }
     keys_array.each do | k |
@@ -65,8 +68,8 @@ class HashMap
 
   def remove(key)
     hash_code = hash(key)
-    bucket = hash_code % @@capacity
-    pairs = @@hasharray[bucket]
+    bucket = hash_code % @capacity
+    pairs = @hasharray[bucket]
     pairs.each do |pair|
       if pair[key] != nil
         return pairs.delete(pair)
@@ -76,7 +79,7 @@ class HashMap
 
   def length
     count = 0 
-    @@hasharray.each do |bucket|
+    @hasharray.each do |bucket|
       if bucket == []
         next
       else
@@ -87,13 +90,13 @@ class HashMap
   end
 
   def clear
-    @@hasharray = @@hasharray.map { |value| value = [] }
-    @@hasharray
+    @hasharray = @hasharray.map { |value| value = [] }
+    @hasharray
   end
 
   def keys
     keys_array = []
-    @@hasharray.each do |bucket|
+    @hasharray.each do |bucket|
       if bucket == []
         next
       else
@@ -107,7 +110,7 @@ class HashMap
 
   def values
     values_array = []
-    @@hasharray.each do |bucket|
+    @hasharray.each do |bucket|
       if bucket == []
         next
       else
@@ -121,7 +124,7 @@ class HashMap
 
   def entries
     entries_array = []
-    @@hasharray.each do |bucket|
+    @hasharray.each do |bucket|
       if bucket == []
         next
       else
@@ -139,39 +142,39 @@ class HashMap
 
 end
 
-# test = HashMap.new
-# test.set('apple', 'red')
-# test.set('banana', 'yellow')
-# test.set('carrot', 'orange')
-# test.set('dog', 'brown') #
-# test.set('elephant', 'gray')
-# test.set('frog', 'green')
-# test.set('grape', 'purple') #
-# test.set('hat', 'black')
-# test.set('ice cream', 'white')
-# test.set('jacket', 'blue')
-# test.set('kite', 'pink')
-# test.set('lion', 'golden')
-# puts test.get("lion")
-# puts test.has?("lion")
-# puts "filled buckets is now:"
+test = HashMap.new
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown') #
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple') #
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+puts test.get("lion")
+puts test.has?("lion")
+puts "filled buckets is now:"
+puts test.length
+# puts test.remove("lion")
 # puts test.length
-# # puts test.remove("lion")
-# # puts test.length
-# # test.clear
-# test.keys
-# test.values
-# test.entries
-# binding.pry
-# test.set('moon', 'silver')
-# binding.pry
-# puts "The length is now:"
-# puts test.length
-# puts test.get('moon')
-# puts test.has?('hat')
-# puts test.remove('jacket')
-# puts "the length is now:"
-# puts test.length
-# test.keys
-# test.values
-# test.entries
+# test.clear
+test.keys
+test.values
+test.entries
+binding.pry
+test.set('moon', 'silver')
+binding.pry
+puts "The length is now:"
+puts test.length
+puts test.get('moon')
+puts test.has?('hat')
+puts test.remove('jacket')
+puts "the length is now:"
+puts test.length
+test.keys
+test.values
+test.entries
